f49aa78ec18397d88d4d8965cac85087f483634b
Alex Kaplan <259alex952@gmail.com>
2024-01-11 19:40:29 -0800

    Did Joystick stuff

--- NiftyThrottle/src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ NiftyThrottle/src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -4,21 +4,40 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import java.util.function.Supplier;
+
+import com.revrobotics.*;
+
 public class MotorSubsystem extends SubsystemBase {
+
+  private Supplier<Double> m_leftStickYAxis;
+  private Supplier<Double> m_rightStickYAxis;
   /** Creates a new ExampleSubsystem. */
-  public MotorSubsystem() {}
+  public MotorSubsystem() {
+  Supplier<Double> leftStickYAxis;
+  Supplier<Double> rightStickYAxis;
 
+  leftStickYAxis = m_leftStickYAxis;
+  rightStickYAxis = m_rightStickYAxis;
+  
+  double leftStick = leftStickYAxis.get();
+  double rightStick = rightStickYAxis.get();
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  leftStick = MathUtil.applyDeadband(leftStick, 0.5); 
+  rightStick = MathUtil.applyDeadband(rightStick, 0.5); 
   }
 
   @Override
-  public void simulationPeriodic() {
+  public void periodic() {
+    
+  }
+
+  @Override
+  public void simulationPeriodic() {  
     // This method will be called once per scheduler run during simulation
   }
 }
